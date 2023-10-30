0d0cc5f526b06f134ce205cbf974c70b0aca1d78
Mike Leonides <mleonides@k2united.com>
2023-10-30 16:42:41 -0500

    First commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>My Timer</h1>
+  <div><input id="seconds" type="number" v-model="seconds" />seconds</div>
+  <button @click="startTimer">Start</button>
+  <p>Time is currently {{ seconds }}</p>
+</template>
+
+<script></script>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  display: flex;
+  place-items: center;
+  flex-direction: column;
+}
+
+@media (min-width: 1024px) {
+  header {
+    padding-right: calc(var(--section-gap) / 2);
+  }
+}
+</style>
